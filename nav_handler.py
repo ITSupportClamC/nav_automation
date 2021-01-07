507473e847ab9fd55b46555547176b3b6ca9367e
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2021-01-07 12:16:19 +0800

    first commit

---
+++ nav_handler.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+#
+# Functions needed to calculate IMA yield
+# 
+from os.path import abspath, dirname
+
+
+
+def getSTBFNavDataFromFile(file):
+	"""
+	[String] file (daily NAV file for Short Term Bond Fund)
+		=> [Iterable] (date, class, currency, number of unit, nav)
+	"""
+	# FIXME: to be implemented
+	return []
+
+
+
+def updateWebSite(mode, timeOut, fundName, navData):
+	"""
+	[String] mode (0 means production, 1 means test)
+	[String] timeout (in miliseconds)
+	[String] fund name,
+	[Tuple] nav Data (date, class, currency, nav)
+
+	In production mode (0), update the website in production.
+	In test mode (1), update the test website.
+
+	Web sites (production, test) and login credentials should be
+	configurable in a file.
+
+	If timed out when trying to update the web site, throw an exception.
+
+	return 0 when successful, throw exception otherwise.
+	"""
+	# FIXME: to be implemented
+	return 0
+
+
+
+"""
+	returns the current directory
+
+	for running the test case provided
+"""
+getCurrentDirectory = lambda : \
+	dirname(abspath(__file__))
+
+
+
---
+++ tests/test_nav_handler_steven.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# 
+
+import unittest2
+from nav_automation.nav_handler import getCurrentDirectory \
+									, getSTBFNavDataFromFile
+from os.path import join
+
+
+
+class TestNavHandlerSteven(unittest2.TestCase):
+
+	def __init__(self, *args, **kwargs):
+		super(TestNavHandlerSteven, self).__init__(*args, **kwargs)
+
+
+	def testGetSTBFNavDataFromFile(self):
+		file = join(getCurrentDirectory(), 'samples', 'sample PriceSTBF.xls')
+		data = getSTBFNavDataFromFile(file)
+		sortedData = sorted(data, key=lambda t: t[1])
+		self.assertEqual(2, len(sortedData))
+		self.assertEqual( ('2020-12-30', 'Class B', 'USD', 400000, 9.9961)
+						, sortedData[0])
+		self.assertEqual( ('2020-12-30', 'Class I', 'USD', 4500000, 9.9959)
+						, sortedData[0])
